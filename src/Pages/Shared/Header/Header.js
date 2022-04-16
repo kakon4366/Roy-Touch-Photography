677f3f93825865999b3d89fa305cf831a94d08ab
kakon4366 <kakonbarman4366@gmail.com>
2022-04-17 00:31:45 +0600

    Tailwind css install

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<div className="container mx-auto">
+				<h1>Hello</h1>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
